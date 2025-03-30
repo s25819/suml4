243f5797259212026f4a4f4c8e23e0625b273e03
s25819 <s25819@pjwstk.edu.pl>
2025-03-30 13:45:20 +0200

    final version

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import streamlit as st
 import pickle
+import sys
 from datetime import datetime
 startTime = datetime.now()
 # import znanych nam bibliotek
@@ -9,28 +10,33 @@ startTime = datetime.now()
 import pathlib
 from pathlib import Path
 
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+temp = pathlib.PosixPath if (sys.platform == "darwin") else pathlib.WindowsPath
+#pathlib.PosixPath = pathlib.WindowsPath
 
 filename = "model.sv"
 model = pickle.load(open(filename,'rb'))
 # otwieramy wcześniej wytrenowany model
 
-pclass_d = {0:"Pierwsza",1:"Druga", 2:"Trzecia"}
+pclass_d = {1:"Pierwsza",2:"Druga", 3:"Trzecia"}
 embarked_d = {0:"Cherbourg", 1:"Queenstown", 2:"Southampton"}
+sex_d = {0: "Męzczyzna", 1: "Kobieta"}
 # o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
 
 def main():
 
-	st.set_page_config(page_title="???")
+	page_title = "Czy przezylbys na Titanicu"
+	panel_title = "Kryteria"
+	titanic_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/2560px-RMS_Titanic_3.jpg"
+
+	st.set_page_config(page_title=page_title)
 	overview = st.container()
 	left, right = st.columns(2)
 	prediction = st.container()
 
-	st.image("https://i.kym-cdn.com/entries/icons/mobile/000/026/489/crying.jpg")
+	st.image(titanic_image)
 
 	with overview:
-		st.title("???")
+		st.title(panel_title)
 
 	with left:
 		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
@@ -40,9 +46,11 @@ def main():
 		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=100)
 		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
 		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=10)
-		fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=1)
+		fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=1)	
+		pclass_radio = st.radio("Klasa biletu", list(pclass_d.keys()), format_func= lambda x: pclass_d[x])
 
 	data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+	print(f"Dane przekazane do modelu: {data}")
 	survival = model.predict(data)
 	s_confidence = model.predict_proba(data)
 
